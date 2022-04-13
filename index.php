6f8b091c275c54c04c5595c64acc9913b5ef123d
joel-irineu <irineu.joel01@gmail.com>
2022-04-13 14:40:00 -0300

    criando menu e página do plugin

--- index.php
+++ index.php
@@ -10,8 +10,23 @@
 
     //  criando shortcode
     function myShortcode() {
-        echo '<h1>Meu Plugin</h1>';
+        $shortCode = '<h1>Meu Plugin</h1>';
+        return $shortCode;
     }
     // [myShortcode]
     add_shortcode('irineu1', 'myShortcode');
+
+    // criando menu
+    function myMenu() {
+        add_menu_page('Meu Plugin', 'Meu Plugin', 'manage_options', 'meu-plugin-wp', 'myPluginPage', 'https://projetos.joelirineu.com.br/plugin/wp-content/uploads/2022/04/plugin.png', 200);
+    }
+    add_action('admin_menu', 'myMenu');
+
+    function myPluginPage() {
+        echo '
+        <div class="wrap">
+            <h1>Bem Vindo(a) ao Meu Plugin</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem!</p>
+        ';
+    }
 ?>
