80bf9af9fd61843827779f374ec64b823759ee95
irineu <irineu.joel01@gmail.com>
2022-04-13 14:03:16 -0300

    Adicionando cabeçalho e dados do plugin

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+    /**
+     * Plugin Name: Meu Plugin Wp
+     * Plugin URI: https://github.com/Joel-Irineu/meu-plugin-wp
+     * Description: Plugin criado em estudo
+     * Author: Joel Irineu
+     * Author URI: https://joelirineu.com.br
+     * Version: 1.0
+     */
